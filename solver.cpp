8348fb485bfdd927f790945992eb5a673c48069a
Lucaskywalker <lucas.renan.vieira@gmail.com>
2017-11-29 12:26:25 -0200

    Solver atualizado

--- solver.cpp
+++ solver.cpp
@@ -21,11 +21,37 @@ void Solver::solve(Simplex PL) {
     PL.started = true;
     vector<bool> b = PLAuxiliar.base;
     vector<int> p = PLAuxiliar.pos;
+    vector<int> falta;
+    for(int i = 1; i < PL.A.size(); i++) {
+      for(int j = 0; j < PL.A[i].size(); j++) {
+        PL.A[i][j] = PLAuxiliar.A[i][j];
+      }
+    }
+    for(int i = 1; i < PL.A.size(); i++) {
+      PL.A[i][PL.b_col] = PLAuxiliar.A[i][PLAuxiliar.b_col];
+    }
     for(int i = 0; i < b.size(); i++) {
       if(b[i]) {
+        if(i >= PL.A.size()) {
+          falta.push_back(p[i]);
+          continue;
+        }
         PL.base[i] = true;
         PL.pos[i] = p[i];
+        cout<<PL.pos[i]<<" "<<i<<" BASE"<<endl;
         PL.pivot(p[i], i);
+      }
+    }
+    for(int i = 0; i < falta.size(); i++) {
+      int row = falta[i];
+      for(int j = 0; j < PL.n_variables; j++) {
+        if(!(PL.A[row][j] == 0)) {
+          PL.base[j] = true;
+          cout<<"virou base "<<row<<" "<<j<<endl;
+          PL.pos[j] = row;
+          PL.pivot(row, j);
+          break;
+        }
       }
     }
     PL.solvePrimal();
